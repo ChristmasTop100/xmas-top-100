81ac1bcbc64c84bcd2482ee3b2731711b3321c16
Robin Drost <robindrost@gmail.com>
2015-11-25 21:19:56 +0100

    Removed the spotify client ID and secret from the config file. Added the default .env.example since it was missing.

--- config/spotify.php
+++ config/spotify.php
@@ -5,9 +5,11 @@
  */
 return [
 	
-	'client_id' 	=> env('SPOTIFY_CLIENT_ID', '01c78bdec97245d195b048900fb8d0c2'),
-	'client_secret' => env('SPOTIFY_CLIENT_SECRET', '910f1bc43c8148b1b85581cbc38ddada'),
+	// Ask / generate these.
+	'client_id' 	=> env('SPOTIFY_CLIENT_ID', ''),
+	'client_secret' => env('SPOTIFY_CLIENT_SECRET', ''),
 
+	// Default user and playlist.
 	'user_id' 		=> 'robindrost',
 	'playlist_id' 	=> '1qmMthdPjXr8wr6Qe1cjOe',
 
