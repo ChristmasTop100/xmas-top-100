d23d6f45c3c46f2b620b555818d70133a997b9e0
Robin Drost <robindrost@gmail.com>
2015-11-25 15:11:52 +0100

    Added spotify config file and composer packages.

---
+++ config/spotify.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Spotify API credentials.
+ */
+return [
+	
+	'client_id' 	=> env('SPOTIFY_CLIENT_ID', 'SomeRandomString'),
+	'client_secret' => env('SPOTIFY_CLIENT_SECRET', 'SomeRandomString'),
+
+];
