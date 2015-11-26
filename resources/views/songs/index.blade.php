4e8ac8ecd92274f04216d8d07b72b2369e263df1
Robin Drost <robindrost@gmail.com>
2015-11-26 12:57:17 +0100

    Updated front-end

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -15,14 +15,14 @@
 	<div class="container songs">
 
 		@foreach ($songs as $song)
-			<div class="row">
+			<div class="row" data-id="{{ $song->id }}">
 				<div class="col-sm-1">
 					<div class="cover" style="background-image: url({{ $song->image }})">
-
+						<div class="overlay"></div>
 					</div>
 				</div>
 
-				<div class="col-sm-1">
+				<div class="col-sm-1 text-center">
 					<a target="_blank" class="play-wrapper" href="{{ $song->url }}">
 						<span class="play"></span>
 					</a>
@@ -33,6 +33,9 @@
 				</div>
 
 				<div class="col-sm-2 vote text-right">
+					<span class="counter">
+						
+					</span>
 					<a href="#"><i class="ion-plus-round"></i></a>
 					<a href="#"><i class="ion-minus-round"></i></a>
 				</div>
@@ -41,4 +44,46 @@
 
 	</div>
 
+@stop
+
+@section('footer')
+	
+@stop
+
+@section('scripts')
+	<script>
+		// (function ($) {
+		// 	var element, totalVotes = 100, count, counter, id;
+
+		// 	$('.songs').find('.row').each(function (i, el) {
+		// 		element = $(e);
+		// 		counter = $('.counter', element);
+		// 		count 	= counter.html();
+		// 		id 		= element.data('id');
+
+		// 		$('.vote-plus', element).on('click', function (e) {
+		// 			e.preventDefault();
+
+		// 			if (totalVotes > 0) {
+		// 				count++;
+		// 				totalVotes--;
+
+		// 				counter.html(count);
+		// 			}
+		// 		});
+
+		// 		$('.vote-minus', element).on('click', function (e) {
+		// 			e.preventDefault();
+
+		// 			if (count > 0 && totalVotes < 100) {
+		// 				count--;
+		// 				totalVotes++;
+
+		// 				counter.html(count);
+		// 			}
+		// 		});
+		// 	});
+			
+		// })(jQuery);
+	</script>
 @stop
