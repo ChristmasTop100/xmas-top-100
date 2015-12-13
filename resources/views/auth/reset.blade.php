6d50340262f08f38b48945bb2d0881066293b355
Yaron Tal <yaron.tal@oneshoe.nl>
2015-12-13 22:36:59 +0100

    Make the one time login form look a bit more like the rest of the site

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,35 +1,49 @@
-<!-- resources/views/auth/reset.blade.php -->
+@extends('layouts.default')
 
-<form method="POST" action="/auth/otl">
-    {!! csrf_field() !!}
-    <input type="hidden" name="token" value="{{ $token }}">
+@section('header')
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1>Christmastop <span class="green">100</span></h1>
+			</div>
+		</div>
+	</div>
+@stop
 
-    @if (count($errors) > 0)
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+@section('content')
+  <div class="container">
+    <form method="POST" action="/auth/otl">
+        {!! csrf_field() !!}
+        <input type="hidden" name="token" value="{{ $token }}">
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+        @if (count($errors) > 0)
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
 
-    <div>
-        Password
-        <input type="password" name="password">
-    </div>
+        <div>
+            Email
+            <input type="email" name="email" value="{{ old('email') }}">
+        </div>
 
-    <div>
-        Confirm Password
-        <input type="password" name="password_confirmation">
-    </div>
+        <div>
+            Password
+            <input type="password" name="password">
+        </div>
 
-    <div>
-        <button type="submit">
-            Reset Password
-        </button>
-    </div>
-</form>
+        <div>
+            Confirm Password
+            <input type="password" name="password_confirmation">
+        </div>
+
+        <div>
+            <button type="submit">
+                Reset Password
+            </button>
+        </div>
+    </form>
+  </div>
+@stop
