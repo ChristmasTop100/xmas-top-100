38a4260544beef2fe91cfaf1fd752ba81fd60617
Yaron Tal <yaron.tal@oneshoe.nl>
2015-12-14 16:55:06 +0100

    Fixed static tokens in activation mails

--- resources/views/emails/password_raw.blade.php
+++ resources/views/emails/password_raw.blade.php
@@ -2,7 +2,7 @@
 
 {{ trans('onetimelogin.salutation', ['name' => $user->name]) }}
 
-{{ trans('onetimelogin.token_text', ['otl_link' =>  'http://christmastop100.nl/auth/otl/4edef50a6ec2382088925b873b16f0899da6210970afbb6697bb68a8e0e2054a']) }}
+{{ trans('onetimelogin.token_text', ['otl_link' =>  'http://christmastop100.nl/auth/otl/' . $token]) }}
 
 {{ trans('onetimelogin.closing') }}
 {{ trans('onetimelogin.sender_name') }}
