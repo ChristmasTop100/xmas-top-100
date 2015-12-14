c9d5f687e71d74dec66feb15d93f99f47509e1b5
Yaron Tal <yaron.tal@oneshoe.nl>
2015-12-14 16:19:22 +0100

    Added a text-only version of the activation mail

---
+++ resources/views/emails/password_raw.blade.php
@@ -0,0 +1,8 @@
+{{ trans('onetimelogin.title') }}
+
+{{ trans('onetimelogin.salutation', ['name' => $user->name]) }}
+
+{{ trans('onetimelogin.token_text', ['otl_link' =>  'http://christmastop100.nl/auth/otl/4edef50a6ec2382088925b873b16f0899da6210970afbb6697bb68a8e0e2054a']) }}
+
+{{ trans('onetimelogin.closing') }}
+{{ trans('onetimelogin.sender_name') }}
